278e26b1ce1f13f541219f4b4e42b9243bb136fe
jaychen 陈家有 <jaychen@hellotalk.com>
2021-12-14 10:38:56 +0800

    新增工具类widgetUtil 创建一个带有毛玻璃背景的widget

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,3 +6,9 @@
 
 ## [0.0.6] - TODO: 2021/12/6.
 *  新增自定义OverScrollBehavior去除listview水波纹 修复0.0.5版本changeLog版本时间错误问题
+
+## [0.0.7] - TODO: 2021/12/6.
+*  新增下拉弹窗效果 源码取自：https://pub.flutter-io.cn/packages/gzx_dropdown_menu/versions
+
+## [0.0.8] - TODO: 2021/12/6.
+*  新增工具类widgetUtil 创建一个带有毛玻璃背景的widget
---
+++ lib/widget/widget_util.dart
@@ -0,0 +1,71 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class WidgetUtil {
+  ///创建一个带有毛玻璃背景的widget
+  static Widget createGroundGlass({
+    Widget background,
+    Widget child,
+    double width,
+    double height,
+    double topLeft = 0,
+    double topRight = 0,
+    double bottomLeft = 0,
+    double bottomRight = 0,
+    double opacity = 0.2,
+    double sigmaX = 5.0,
+    double sigmaY = 5.0,
+    Color glassColor,
+  }) {
+    ///圆角
+    BorderRadius borderRadius = BorderRadius.only(
+      topLeft: Radius.circular(topLeft),
+      topRight: Radius.circular(topRight),
+      bottomLeft: Radius.circular(bottomLeft),
+      bottomRight: Radius.circular(bottomRight),
+    );
+
+    ///毛玻璃背景
+    BackdropFilter backdropFilter = BackdropFilter(
+      filter: ImageFilter.blur(sigmaX: sigmaX, sigmaY: sigmaY),
+      child: Opacity(
+        opacity: opacity,
+        child: Container(
+          decoration: BoxDecoration(
+            color: glassColor ?? Color(0x80FFFFFF),
+          ),
+        ),
+      ),
+    );
+
+    return Container(
+      width: width,
+      height: height,
+      child: Stack(
+        children: <Widget>[
+          //约束性盒子
+          ConstrainedBox(
+            constraints: const BoxConstraints.expand(),
+            child: ClipRRect(
+              child: background,
+              borderRadius: borderRadius,
+            ),
+          ),
+          Center(
+            //可裁切的矩形
+            child: ClipRRect(
+              borderRadius: borderRadius,
+              //背景过滤器
+              child: backdropFilter,
+            ),
+          ),
+          Center(
+            child: child,
+          ),
+        ],
+      ),
+    );
+  }
+}
