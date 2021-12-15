408f03637ec8ef4a52b1a398e9fa4dd74d3e8e4b
jaychen 陈家有 <jaychen@hellotalk.com>
2021-12-15 12:15:58 +0800

    新增WrapWidget自动换行排版widget

---
+++ lib/widget/wrap_widget.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+/*
+* 可以让子控件自动换行的控件
+*
+* */
+
+typedef Widget WrapItemWidget<T>(T value, int index);
+
+class WrapWidget<T> extends StatelessWidget {
+  final List<T> data;
+  final double spacing;
+  final double runSpacing;
+  final WrapItemWidget itemWidget;
+
+  const WrapWidget(
+      {Key key, this.data, this.spacing = 12, this.runSpacing = 12, this.itemWidget})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    /*一个渐变颜色的正方形集合*/
+    List<Widget> children() => List.generate(data.length, (index) {
+          return itemWidget(data[index], index);
+        }).cast();
+
+    return Wrap(
+      spacing: spacing, //主轴上子控件的间距
+      runSpacing: runSpacing, //交叉轴上子控件之间的间距
+      children: children(), //要显示的子控件集合
+    );
+  }
+}
